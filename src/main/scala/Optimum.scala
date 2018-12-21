c7860bc6bfe8226b6e01bc0aa3282503c6d8f9c8
Marc Wautelet <marc.wautelet@skynet.be>
2018-12-21 01:34:32 +0100

    Next and drop working too

--- src/main/scala/Optimum.scala
+++ src/main/scala/Optimum.scala
@@ -1,4 +1,3 @@
-import Optimum.company
 import mw.optimum.model.Company
 import mw.optimum.view.{Graph, GraphPane}
 import scalafx.application.JFXApp
@@ -7,74 +6,101 @@ import scalafx.scene.Scene
 import scalafx.scene.control.{Button, ScrollPane, TextField}
 import scalafx.scene.layout.{BorderPane, HBox}
 import scalafx.stage.FileChooser
-
 import scala.util.Try
 
 object Optimum extends JFXApp {
-  var undoStack = List.empty[Company]
-  var company = Company.empty
-  var redoStack = List.empty[Company]
-  val graphPane = new GraphPane
-  stage = new PrimaryStage {
-    title = "Optimum"
-    scene = new Scene {
-      root = new BorderPane {
-        top = new HBox {
-          val undoButton = new Button {
-            text = "Undo"
-            disable = true
-            onAction = { _ =>
-              redoStack ::= company
-              company = undoStack.head
-              undoStack = undoStack.tail
-              refresh()
-            }
-          }
-          val redoButton = new Button {
-            text = "Redo"
-            disable = true
-            onAction = { _ =>
-              undoStack ::= company
-              company = redoStack.head
-              redoStack = redoStack.tail
-              refresh()
-            }
-          }
-          val nextButton = new Button {
-            text = "Next"
-            disable = true
-          }
-          val sizeField = new TextField {
-            text = "7"
-          }
-          val loadButton = new Button("Load") {
-            onAction = { _ =>
-              val chooser = new FileChooser
-              chooser.showOpenDialog(stage) match {
-                case null =>
-                case file =>
-                  undoStack = Nil
-                  company = Company.loadFrom(file)
-                  redoStack = company.start :: Nil
-                  refresh()
-              }
-            }
-          }
-          children = loadButton :: undoButton :: redoButton :: nextButton :: sizeField :: Nil
-          def refresh(): Unit = {
-            val maxTribeSize = Try(sizeField.text().toInt).getOrElse(0)
-            val previousGraph = graphPane.currentGraph
-            val newGraph = Graph(company, maxTribeSize, previousGraph)
-            graphPane.show(newGraph)
-            undoButton.disable = undoStack.isEmpty
-            redoButton.disable = redoStack.isEmpty
-            nextButton.disable = newGraph.mostCoupled.isEmpty
-          }
-        }
-        center = new ScrollPane {
-          content = graphPane
-        }
-      }
-    }
-  }
+	var undoStack = List.empty[Company]
+	var company = Company.empty
+	var redoStack = List.empty[Company]
+	val graphPane = new GraphPane
+	stage = new PrimaryStage {
+		title = "Optimum"
+		scene = new Scene {
+			root = new BorderPane {
+				top = new HBox {
+					val sizeField = new TextField {
+						text = "7"
+					}
+					val loadScoreField = new TextField
+					val currentScoreField = new TextField
+					val bestScoreField = new TextField
+					val loadButton = new Button("Load") {
+						onAction = { _ =>
+							val chooser = new FileChooser
+							chooser.showOpenDialog(stage) match {
+								case null =>
+								case file =>
+									undoStack = Nil
+									company = Company.loadFrom(file)
+									redoStack = Nil
+									loadScoreField.text = s"${company.score}"
+									refresh()
+							}
+						}
+					}
+					val undoButton = new Button {
+						text = "Undo"
+						disable = true
+						onAction = { _ =>
+							redoStack ::= company
+							company = undoStack.head
+							undoStack = undoStack.tail
+							refresh()
+						}
+					}
+					val redoButton = new Button {
+						text = "Redo"
+						disable = true
+						onAction = { _ =>
+							undoStack ::= company
+							company = redoStack.head
+							redoStack = redoStack.tail
+							refresh()
+						}
+					}
+					val dropButton = new Button {
+						text = "Drop Tribes"
+						disable = true
+						onAction = { _ =>
+							undoStack ::= company
+							company = company.start
+							redoStack = Nil
+							refresh()
+						}
+					}
+					val nextButton = new Button {
+						text = "Next"
+						disable = true
+						onAction = { _ =>
+							company.next(maxTribeSize) match {
+								case Some(next) =>
+									undoStack ::= company
+									company = next
+									redoStack = Nil
+									refresh()
+								case _ =>
+							}
+						}
+					}
+					children = loadButton :: undoButton :: redoButton :: dropButton :: nextButton :: sizeField ::
+						loadScoreField :: currentScoreField :: bestScoreField :: Nil
+					def refresh(): Unit = {
+						val maxTribeSize = Try(sizeField.text().toInt).getOrElse(0)
+						val previousGraph = graphPane.currentGraph
+						val newGraph = Graph(company, maxTribeSize, previousGraph)
+						graphPane.show(newGraph)
+						undoButton.disable = undoStack.isEmpty
+						redoButton.disable = redoStack.isEmpty
+						nextButton.disable = newGraph.mostCoupled.isEmpty
+						dropButton.disable = !company.tribes.exists(_.size > 1)
+						currentScoreField.text = s"${company.score}"
+					}
+					def maxTribeSize = Try(sizeField.text().toInt).getOrElse(0)
+				}
+				center = new ScrollPane {
+					content = graphPane
+				}
+			}
+		}
+	}
 }
--- src/main/scala/mw/optimum/model/Company.scala
+++ src/main/scala/mw/optimum/model/Company.scala
@@ -1,88 +1,94 @@
 package mw.optimum.model
 
 import java.io.{File, FileInputStream}
-
 import scala.io.Source
 import scala.util.parsing.combinator.RegexParsers
 
 trait Company {
-  def tribes: List[Tribe]
-  def tribe(name: String) = tribes.find(_.name == name)
-  def start = {
-    val dummyTribes = for {
-      tribe <- tribes
-      squad <- tribe.squads
-    } yield Tribe(squads = squad :: Nil)
-    Company(dummyTribes)
-  }
-  def maxWeight(maxTribeSize: Int) = {
-    val weights = for {
-      tribe1 <- tribes
-      tribe2 <- tribes
-      if tribe1 != tribe2 && tribe1.size + tribe2.size <= maxTribeSize
-    } yield tribe1 <-> tribe2
-    if (weights.isEmpty) 0 else weights.max
-  }
-  def mostCoupled(maxTribeSize: Int) = {
-    val maxWeight = this.maxWeight(maxTribeSize)
-    for {
-      tribe1 <- tribes
-      tribe2 <- tribes
-      weight = tribe1 <-> tribe2
-      if tribe1 != tribe2 && weight == maxWeight && tribe1.size + tribe2.size <= maxTribeSize
-    } yield (tribe1, weight, tribe2)
-  }
-  def next(maxTribeSize: Int) =
-    for ((tribe1, _, tribe2) <- mostCoupled(maxTribeSize).headOption) yield {
-      val newTribe = tribe1 ++ tribe2
-      this - tribe1 - tribe2 + newTribe
-    }
-  def +(tribe: Tribe) = Company(tribe :: tribes)
-  def -(tribe: Tribe) = Company(tribes.filter(_ != tribe))
-  override def toString = s"Company(${tribes.size} tribes)"
+	def tribes: List[Tribe]
+	def tribe(name: String) = tribes.find(_.name == name)
+	def start = {
+		val dummyTribes = for {
+			tribe <- tribes
+			squad <- tribe.squads
+		} yield Tribe(squads = squad :: Nil)
+		Company(dummyTribes)
+	}
+	def maxWeight(maxTribeSize: Int) = {
+		val weights = for {
+			tribe1 <- tribes
+			tribe2 <- tribes
+			if tribe1 != tribe2 && tribe1.size + tribe2.size <= maxTribeSize
+		} yield tribe1 <-> tribe2
+		if (weights.isEmpty) 0 else weights.max
+	}
+	def mostCoupled(maxTribeSize: Int) = {
+		val maxWeight = this.maxWeight(maxTribeSize)
+		for {
+			tribe1 <- tribes
+			tribe2 <- tribes
+			weight = tribe1 <-> tribe2
+			if tribe1 != tribe2 && weight == maxWeight && tribe1.size + tribe2.size <= maxTribeSize
+		} yield (tribe1, weight, tribe2)
+	}
+	def next(maxTribeSize: Int) =
+		for ((tribe1, _, tribe2) <- mostCoupled(maxTribeSize).headOption) yield {
+			val newTribe = tribe1 ++ tribe2
+			this - tribe1 - tribe2 + newTribe
+		}
+	def score = {
+		val weights = for {
+			tribe1 <- tribes
+			tribe2 <- tribes if tribe1 != tribe2
+		} yield tribe1 <-> tribe2
+		weights.sum
+	}
+	def +(tribe: Tribe) = Company(tribe :: tribes)
+	def -(tribe: Tribe) = Company(tribes.filter(_ != tribe))
+	override def toString = s"Company(${tribes.size} tribes)"
 }
 object Company extends RegexParsers {
-  val empty: Company = new Company {
-    val tribes = Nil
-  }
-  def loadFrom(file: File): Company = {
-    val input = Source.fromInputStream(new FileInputStream(file)).getLines.drop(1)
-    val company = new Company.Mutable
-    for (line <- input) {
-      parseAll(record, line) match {
-        case Success((t1, s1, w, s2, t2), _) =>
-          val tribe1 = company.tribe(t1).getOrElse(new Tribe.Mutable(t1))
-          val tribe2 = company.tribe(t2).getOrElse(new Tribe.Mutable(t2))
-          val squad1 = tribe1.squad(s1).getOrElse(new Squad.Mutable(s1))
-          val squad2 = tribe2.squad(s2).getOrElse(new Squad.Mutable(s2))
-          squad1 += squad2 -> w
-          squad2 += squad1 -> w
-          tribe1 += squad1
-          tribe2 += squad2
-          company += tribe1
-          company += tribe2
-        case NoSuccess(msg, _) =>
-          throw new FileFormatException(msg)
-      }
-    }
-    company
-  }
-  class Mutable extends Company {
-    var tribes = List.empty[Tribe.Mutable]
-    override def tribe(name: String) = tribes.find(_.name == name)
-    def +=(tribe: Tribe.Mutable) =
-      if (!tribes.contains(tribe))
-        tribes ::= tribe
-  }
-  class Immutable(val tribes: List[Tribe]) extends Company
-  def apply(tribes: List[Tribe] = Nil): Company = new Immutable(tribes)
-  def record: Parser[(String, String, Int, String, String)] =
-    field ~ "," ~ field ~ "," ~ field ~ "," ~ field ~ "," ~ field ^^ {
-      case t1 ~ _ ~ s1 ~ _ ~ w ~ _ ~ s2 ~ _ ~ t2 =>
-        (t1, s1, w.toInt, s2, t2)
-    }
-  def field: Parser[String] = quoted | unquoted
-  def quoted: Parser[String] ="""("[^"]*")+""".r ^^ (_.drop(1).dropRight(1).replaceAllLiterally("\"\"", "\""))
-  def unquoted: Parser[String] ="""[^,]*""".r
-  class FileFormatException(msg: String) extends Exception(msg)
+	val empty: Company = new Company {
+		val tribes = Nil
+	}
+	def loadFrom(file: File): Company = {
+		val input = Source.fromInputStream(new FileInputStream(file)).getLines.drop(1)
+		val company = new Company.Mutable
+		for (line <- input) {
+			parseAll(record, line) match {
+				case Success((t1, s1, w, s2, t2), _) =>
+					val tribe1 = company.tribe(t1).getOrElse(new Tribe.Mutable(t1))
+					val tribe2 = company.tribe(t2).getOrElse(new Tribe.Mutable(t2))
+					val squad1 = tribe1.squad(s1).getOrElse(new Squad.Mutable(s1))
+					val squad2 = tribe2.squad(s2).getOrElse(new Squad.Mutable(s2))
+					squad1 += squad2 -> w
+					squad2 += squad1 -> w
+					tribe1 += squad1
+					tribe2 += squad2
+					company += tribe1
+					company += tribe2
+				case NoSuccess(msg, _) =>
+					throw new FileFormatException(msg)
+			}
+		}
+		company
+	}
+	class Mutable extends Company {
+		var tribes = List.empty[Tribe.Mutable]
+		override def tribe(name: String) = tribes.find(_.name == name)
+		def +=(tribe: Tribe.Mutable) =
+			if (!tribes.contains(tribe))
+				tribes ::= tribe
+	}
+	class Immutable(val tribes: List[Tribe]) extends Company
+	def apply(tribes: List[Tribe] = Nil): Company = new Immutable(tribes)
+	def record: Parser[(String, String, Int, String, String)] =
+		field ~ "," ~ field ~ "," ~ field ~ "," ~ field ~ "," ~ field ^^ {
+			case t1 ~ _ ~ s1 ~ _ ~ w ~ _ ~ s2 ~ _ ~ t2 =>
+				(t1, s1, w.toInt, s2, t2)
+		}
+	def field: Parser[String] = quoted | unquoted
+	def quoted: Parser[String] ="""("[^"]*")+""".r ^^ (_.drop(1).dropRight(1).replaceAllLiterally("\"\"", "\""))
+	def unquoted: Parser[String] ="""[^,]*""".r
+	class FileFormatException(msg: String) extends Exception(msg)
 }
