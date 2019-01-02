c9ae44522e104718cb595e4a63fb113d8948a8cf
Allaess <marc.wautelet@skynet.be>
2019-01-02 14:48:02 +0100

    Moved text fields to bottom of window ("status text") Implemented save button.

--- src/main/scala/Optimum.scala
+++ src/main/scala/Optimum.scala
@@ -8,174 +8,186 @@ import scalafx.scene.layout.{BorderPane, HBox}
 import scalafx.stage.FileChooser
 
 object Optimum extends JFXApp {
-	var company = Company.empty
-	var maxTribeSize = 7
-	var (nextCouple, bestScore) = company.nextCoupleAndBestScore(maxTribeSize)
-	var loadScore = 0
-	var undoStack = List.empty[Company]
-	var redoStack = List.empty[Company]
-	var scale = 1.0
-	def name(tribe: Tribe) = if (tribe.name.startsWith("Tribe")) {
-		if (tribe.size == 1) s"${tribe.squads.head.name}"
-		else {
-			val prefixes = tribe.squads.map(_.name.take(3)).distinct
-			val sorted = prefixes.sortBy { prefix =>
-				prefixes.count(_ == prefix)
-			}.reverse
-			s"${tribe.name} (${sorted.mkString(",")})"
-		}
-	} else tribe.name
-	def sizeText = s" $maxTribeSize squads per tribe "
-	def loadScoreText = s"     Start: $loadScore     "
-	def currentScoreText = s"Current: ${company.score}     "
-	def bestScoreText = s"Best: $bestScore     "
-	def dependenciesText = nextCouple match {
-		case Some((tribe1, weight, tribe2)) =>
-			val prefix = if (weight == 1) "1 dependency" else s"$weight dependencies"
-			val postfix = s"${name(tribe1)} and ${name(tribe2)}"
-			s"$prefix between $postfix     "
-		case None => "No dependencies     "
-	}
-	stage = new PrimaryStage {
-		title = "Optimum"
-		scene = new Scene {
-			root = new BorderPane {
-				val graphPane = GraphPane(merge)
-				val sizeField = new Label {
-					text = sizeText
-				}
-				val loadScoreField = new Label {
-					text = loadScoreText
-				}
-				val currentScoreField = new Label {
-					text = currentScoreText
-				}
-				val bestScoreField = new Label {
-					text = bestScoreText
-				}
-				val dependenciesCount = new Label {
-					text = dependenciesText
-				}
-				val lessSquadsButton = new Button("-") {
-					onAction = { _ =>
-						maxTribeSize -= 1
-						refresh()
-					}
-				}
-				val moreSquadsButton = new Button("+") {
-					onAction = { _ =>
-						maxTribeSize += 1
-						refresh()
-					}
-				}
-				val loadButton = new Button("Load") {
-					onAction = { _ =>
-						val chooser = new FileChooser
-						chooser.showOpenDialog(stage) match {
-							case null =>
-							case file =>
-								undoStack = Nil
-								redoStack = Nil
-								company = Company.loadFrom(file)
-								loadScore = company.score
-								refresh()
-						}
-					}
-				}
-				val undoButton = new Button {
-					text = "Undo"
-					disable = true
-					onAction = { _ =>
-						redoStack ::= company
-						company = undoStack.head
-						undoStack = undoStack.tail
-						refresh()
-					}
-				}
-				val redoButton = new Button {
-					text = "Redo"
-					disable = true
-					onAction = { _ =>
-						undoStack ::= company
-						company = redoStack.head
-						redoStack = redoStack.tail
-						refresh()
-					}
-				}
-				val dropButton = new Button {
-					text = "Drop Tribes"
-					disable = true
-					onAction = { _ =>
-						undoStack ::= company
-						company = company.start
-						redoStack = Nil
-						refresh()
-					}
-				}
-				val nextButton = new Button {
-					text = "Next"
-					disable = true
-					onAction = { _ =>
-						nextCouple match {
-							case Some((tribe1, _, tribe2)) =>
-								undoStack ::= company
-								company = company.merge(tribe1, tribe2)
-								redoStack = Nil
-								refresh()
-							case _ =>
-						}
-					}
-				}
-				val zoomInButton = new Button {
-					text = "Zoom in"
-					onAction = { _ =>
-						scale /= 0.75
-						graphPane.scaleX = scale
-						graphPane.scaleY = scale
-						graphPane.translateX = graphPane.width() * (scale - 1) / 2
-						graphPane.translateY = graphPane.height() * (scale - 1) / 2
-					}
-				}
-				val zoomOutButton = new Button {
-					text = "Zoom out"
-					onAction = { _ =>
-						scale *= 0.75
-						graphPane.scaleX = scale
-						graphPane.scaleY = scale
-						graphPane.translateX = graphPane.width() * (scale - 1) / 2
-						graphPane.translateY = graphPane.height() * (scale - 1) / 2
-					}
-				}
-				def merge(tribe1: Tribe, tribe2: Tribe) = {
-					undoStack ::= company
-					company = company.merge(tribe1, tribe2)
-					redoStack = Nil
-					refresh()
-				}
-				def refresh(): Unit = {
-					val (next, best) = company.nextCoupleAndBestScore(maxTribeSize)
-					nextCouple = next
-					bestScore = best
-					sizeField.text = sizeText
-					loadScoreField.text = loadScoreText
-					currentScoreField.text = currentScoreText
-					bestScoreField.text = bestScoreText
-					dependenciesCount.text = dependenciesText
-					undoButton.disable = undoStack.isEmpty
-					redoButton.disable = redoStack.isEmpty
-					dropButton.disable = !company.tribes.exists(_.size > 1)
-					nextButton.disable = nextCouple.isEmpty
-					graphPane.show(company, maxTribeSize)
-				}
-				top = new HBox {
-					children = loadButton :: undoButton :: redoButton :: dropButton :: nextButton ::
-						lessSquadsButton :: sizeField :: moreSquadsButton :: zoomInButton :: zoomOutButton ::
-						loadScoreField :: currentScoreField :: bestScoreField :: dependenciesCount :: Nil
-				}
-				center = new ScrollPane {
-					content = graphPane
-				}
-			}
-		}
-	}
+  var company = Company.empty
+  var maxTribeSize = 7
+  var (nextCouple, bestScore) = company.nextCoupleAndBestScore(maxTribeSize)
+  var loadScore = 0
+  var undoStack = List.empty[Company]
+  var redoStack = List.empty[Company]
+  var scale = 1.0
+  val graphPane = GraphPane(merge)
+  val sizeField = new Label {
+    text = sizeText
+  }
+  val loadScoreField = new Label {
+    text = loadScoreText
+  }
+  val currentScoreField = new Label {
+    text = currentScoreText
+  }
+  val bestScoreField = new Label {
+    text = bestScoreText
+  }
+  val dependenciesCount = new Label {
+    text = dependenciesText
+  }
+  val lessSquadsButton = new Button("-") {
+    onAction = { _ =>
+      maxTribeSize -= 1
+      refresh()
+    }
+  }
+  val moreSquadsButton = new Button("+") {
+    onAction = { _ =>
+      maxTribeSize += 1
+      refresh()
+    }
+  }
+  val loadButton = new Button("Load") {
+    onAction = { _ =>
+      val chooser = new FileChooser
+      chooser.showOpenDialog(stage) match {
+        case null =>
+        case file =>
+          undoStack = Nil
+          redoStack = Nil
+          company = Company.loadFrom(file)
+          loadScore = company.score
+          refresh()
+      }
+    }
+  }
+  val saveButton = new Button("Save") {
+    onAction = { _ =>
+      val chooser = new FileChooser
+      chooser.showSaveDialog(stage) match {
+        case null =>
+        case file =>
+          company.saveTo(file)
+      }
+    }
+  }
+  val undoButton = new Button {
+    text = "Undo"
+    disable = true
+    onAction = { _ =>
+      redoStack ::= company
+      company = undoStack.head
+      undoStack = undoStack.tail
+      refresh()
+    }
+  }
+  val redoButton = new Button {
+    text = "Redo"
+    disable = true
+    onAction = { _ =>
+      undoStack ::= company
+      company = redoStack.head
+      redoStack = redoStack.tail
+      refresh()
+    }
+  }
+  val dropButton = new Button {
+    text = "Drop Tribes"
+    disable = true
+    onAction = { _ =>
+      undoStack ::= company
+      company = company.start
+      redoStack = Nil
+      refresh()
+    }
+  }
+  val nextButton = new Button {
+    text = "Next"
+    disable = true
+    onAction = { _ =>
+      nextCouple match {
+        case Some((tribe1, _, tribe2)) =>
+          undoStack ::= company
+          company = company.merge(tribe1, tribe2)
+          redoStack = Nil
+          refresh()
+        case _ =>
+      }
+    }
+  }
+  val zoomInButton = new Button {
+    text = "Zoom in"
+    onAction = { _ =>
+      scale /= 0.75
+      graphPane.scaleX = scale
+      graphPane.scaleY = scale
+      graphPane.translateX = graphPane.width() * (scale - 1) / 2
+      graphPane.translateY = graphPane.height() * (scale - 1) / 2
+    }
+  }
+  val zoomOutButton = new Button {
+    text = "Zoom out"
+    onAction = { _ =>
+      scale *= 0.75
+      graphPane.scaleX = scale
+      graphPane.scaleY = scale
+      graphPane.translateX = graphPane.width() * (scale - 1) / 2
+      graphPane.translateY = graphPane.height() * (scale - 1) / 2
+    }
+  }
+  def merge(tribe1: Tribe, tribe2: Tribe) = {
+    undoStack ::= company
+    company = company.merge(tribe1, tribe2)
+    redoStack = Nil
+    refresh()
+  }
+  def refresh(): Unit = {
+    val (next, best) = company.nextCoupleAndBestScore(maxTribeSize)
+    nextCouple = next
+    bestScore = best
+    sizeField.text = sizeText
+    loadScoreField.text = loadScoreText
+    currentScoreField.text = currentScoreText
+    bestScoreField.text = bestScoreText
+    dependenciesCount.text = dependenciesText
+    undoButton.disable = undoStack.isEmpty
+    redoButton.disable = redoStack.isEmpty
+    dropButton.disable = !company.tribes.exists(_.size > 1)
+    nextButton.disable = nextCouple.isEmpty
+    graphPane.show(company, maxTribeSize)
+  }
+  def name(tribe: Tribe) = if (tribe.name.startsWith("Tribe")) {
+    if (tribe.size == 1) s"${tribe.squads.head.name}"
+    else {
+      val prefixes = tribe.squads.map(_.name.take(3)).distinct
+      val sorted = prefixes.sortBy { prefix =>
+        prefixes.count(_ == prefix)
+      }.reverse
+      s"${tribe.name} (${sorted.mkString(",")})"
+    }
+  } else tribe.name
+  def sizeText = s" $maxTribeSize squads per tribe "
+  def loadScoreText = s"     Start: $loadScore     "
+  def currentScoreText = s"Current: ${company.score}     "
+  def bestScoreText = s"Best: $bestScore     "
+  def dependenciesText = nextCouple match {
+    case Some((tribe1, weight, tribe2)) =>
+      val prefix = if (weight == 1) "1 dependency" else s"$weight dependencies"
+      val postfix = s"${name(tribe1)} and ${name(tribe2)}"
+      s"$prefix between $postfix     "
+    case None => "No dependencies     "
+  }
+  stage = new PrimaryStage {
+    title = "Optimum"
+    scene = new Scene {
+      root = new BorderPane {
+        top = new HBox {
+          children = loadButton :: saveButton :: undoButton :: redoButton :: dropButton :: nextButton ::
+            lessSquadsButton :: sizeField :: moreSquadsButton :: zoomInButton :: zoomOutButton :: Nil
+        }
+        center = new ScrollPane {
+          content = graphPane
+        }
+        bottom = new HBox {
+          children = loadScoreField :: currentScoreField :: bestScoreField :: dependenciesCount :: Nil
+        }
+      }
+    }
+  }
 }
--- src/main/scala/mw/optimum/model/Company.scala
+++ src/main/scala/mw/optimum/model/Company.scala
@@ -1,111 +1,148 @@
 package mw.optimum.model
 
-import java.io.{File, FileInputStream}
+import java.io._
+
 import scala.io.Source
 import scala.util.parsing.combinator.RegexParsers
 import scala.util.Try
 
 trait Company {
-	def tribes: List[Tribe]
-	def tribe(name: String) = tribes.find(_.name == name)
-	lazy val start = {
-		val dummyTribes = for {
-			tribe <- tribes
-			squad <- tribe.squads
-		} yield Tribe(squads = squad :: Nil)
-		Company(dummyTribes)
-	}
-	def maxWeight(maxTribeSize: Int) = {
-		val weights = for {
-			tribe1 <- tribes
-			tribe2 <- tribes
-			if tribe1.name < tribe2.name && tribe1.size + tribe2.size <= maxTribeSize
-		} yield tribe1 <-> tribe2
-		Try(weights.max).getOrElse(0)
-	}
-	def mostCoupled(maxTribeSize: Int) = {
-		val maxWeight = this.maxWeight(maxTribeSize)
-		if (maxWeight == 0) Nil
-		else for {
-			tribe1 <- tribes
-			tribe2 <- tribes
-			if tribe1.name < tribe2.name
-			weight = tribe1 <-> tribe2
-			if weight == maxWeight && tribe1.size + tribe2.size <= maxTribeSize
-		} yield (tribe1, weight, tribe2)
-	}
-	def ignored(cutWeight: Int) = for {
-		tribe1 <- tribes
-		tribe2 <- tribes
-		if tribe1.name < tribe2.name
-		weight = tribe1 <-> tribe2
-		if weight > cutWeight
-	} yield (tribe1, weight, tribe2)
-	lazy val score = {
-		val weights = for {
-			tribe1 <- tribes
-			tribe2 <- tribes if tribe1.name < tribe2.name
-		} yield tribe1 <-> tribe2
-		Try(weights.sum).getOrElse(0)
-	}
-	def nextCoupleAndBestScore(maxTribeSize: Int): (Option[(Tribe, Int, Tribe)], Int) = {
-		mostCoupled(maxTribeSize).headOption match {
-			case None => (None, score)
-			case link@Some((tribe1, _, tribe2)) =>
-				val (_, score) = merge(tribe1, tribe2).nextCoupleAndBestScore(maxTribeSize)
-				(link, score)
-		}
-	}
-	def +(tribe: Tribe) = Company(tribe :: tribes)
-	def -(tribe: Tribe) = Company(tribes.filter(_ != tribe))
-	def merge(tribe1: Tribe, tribe2: Tribe) = {
-		val newTribe = if (tribe1.size > tribe2.size) tribe1 ++ tribe2 else tribe2 ++ tribe1
-		this - tribe1 - tribe2 + newTribe
-	}
-	override def toString = s"Company(${tribes.size} tribes)"
+  def tribes: List[Tribe]
+  def tribe(name: String) = tribes.find(_.name == name)
+  lazy val start = {
+    val dummyTribes = for {
+      tribe <- tribes
+      squad <- tribe.squads
+    } yield Tribe(squads = squad :: Nil)
+    Company(dummyTribes)
+  }
+  def maxWeight(maxTribeSize: Int) = {
+    val weights = for {
+      tribe1 <- tribes
+      tribe2 <- tribes
+      if tribe1.name < tribe2.name && tribe1.size + tribe2.size <= maxTribeSize
+    } yield tribe1 <-> tribe2
+    Try(weights.max).getOrElse(0)
+  }
+  def mostCoupled(maxTribeSize: Int) = {
+    val maxWeight = this.maxWeight(maxTribeSize)
+    if (maxWeight == 0) Nil
+    else for {
+      tribe1 <- tribes
+      tribe2 <- tribes
+      if tribe1.name < tribe2.name
+      weight = tribe1 <-> tribe2
+      if weight == maxWeight && tribe1.size + tribe2.size <= maxTribeSize
+    } yield (tribe1, weight, tribe2)
+  }
+  def ignored(cutWeight: Int) = for {
+    tribe1 <- tribes
+    tribe2 <- tribes
+    if tribe1.name < tribe2.name
+    weight = tribe1 <-> tribe2
+    if weight > cutWeight
+  } yield (tribe1, weight, tribe2)
+  lazy val score = {
+    val weights = for {
+      tribe1 <- tribes
+      tribe2 <- tribes if tribe1.name < tribe2.name
+    } yield tribe1 <-> tribe2
+    Try(weights.sum).getOrElse(0)
+  }
+  def nextCoupleAndBestScore(maxTribeSize: Int): (Option[(Tribe, Int, Tribe)], Int) = {
+    mostCoupled(maxTribeSize).headOption match {
+      case None => (None, score)
+      case link@Some((tribe1, _, tribe2)) =>
+        val (_, score) = merge(tribe1, tribe2).nextCoupleAndBestScore(maxTribeSize)
+        (link, score)
+    }
+  }
+  def +(tribe: Tribe) = Company(tribe :: tribes)
+  def -(tribe: Tribe) = Company(tribes.filter(_ != tribe))
+  def merge(tribe1: Tribe, tribe2: Tribe) = {
+    val newTribe = if (tribe1.size > tribe2.size) tribe1 ++ tribe2 else tribe2 ++ tribe1
+    this - tribe1 - tribe2 + newTribe
+  }
+  def saveTo(file: File) = {
+    def format(field: String) = {
+      if (field.contains(",") || field.contains("\"")) s""""${field.replaceAllLiterally("\"", "\"\"")}""""
+      else field
+    }
+    val output = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file)))
+    output.write(",Tribes")
+    for {
+      tribe <- tribes
+      _ <- tribe.squads
+    } output.write(s",${format(tribe.name)}")
+    output.newLine()
+    output.write("Tribes,Squads")
+    for {
+      tribe <- tribes
+      squad <- tribe.squads
+    } output.write(s",${format(squad.name)}")
+    output.newLine()
+    for {
+      tribe1 <- tribes
+      squad1 <- tribe1.squads
+    } {
+      output.write(s"${format(tribe1.name)},${format(squad1.name)}")
+      for {
+        tribe2 <- tribes
+        squad2 <- tribe2.squads
+      } {
+        val weight = squad1 <-> squad2
+        if (weight == 0) output.write(",")
+        else output.write(s",$weight")
+      }
+      output.newLine()
+    }
+    output.flush()
+    output.close()
+  }
+  override def toString = s"Company(${tribes.size} tribes)"
 }
 object Company extends RegexParsers {
-	val empty: Company = new Company {
-		val tribes = Nil
-	}
-	def loadFrom(file: File): Company = {
-		val input = Source.fromInputStream(new FileInputStream(file)).getLines.drop(1)
-		val company = new Company.Mutable
-		for (line <- input) {
-			parseAll(record, line) match {
-				case Success((t1, s1, w, s2, t2), _) =>
-					val tribe1 = company.tribe(t1).getOrElse(new Tribe.Mutable(t1))
-					val tribe2 = company.tribe(t2).getOrElse(new Tribe.Mutable(t2))
-					val squad1 = tribe1.squad(s1).getOrElse(new Squad.Mutable(s1))
-					val squad2 = tribe2.squad(s2).getOrElse(new Squad.Mutable(s2))
-					squad1 += squad2 -> w
-					squad2 += squad1 -> w
-					tribe1 += squad1
-					tribe2 += squad2
-					company += tribe1
-					company += tribe2
-				case NoSuccess(msg, _) =>
-					throw new FileFormatException(msg)
-			}
-		}
-		company
-	}
-	class Mutable extends Company {
-		var tribes = List.empty[Tribe.Mutable]
-		override def tribe(name: String) = tribes.find(_.name == name)
-		def +=(tribe: Tribe.Mutable) =
-			if (!tribes.contains(tribe))
-				tribes ::= tribe
-	}
-	class Immutable(val tribes: List[Tribe]) extends Company
-	def apply(tribes: List[Tribe] = Nil): Company = new Immutable(tribes)
-	def record: Parser[(String, String, Int, String, String)] =
-		field ~ "," ~ field ~ "," ~ field ~ "," ~ field ~ "," ~ field ^^ {
-			case t1 ~ _ ~ s1 ~ _ ~ w ~ _ ~ s2 ~ _ ~ t2 =>
-				(t1, s1, w.toInt, s2, t2)
-		}
-	def field: Parser[String] = quoted | unquoted
-	def quoted: Parser[String] ="""("[^"]*")+""".r ^^ (_.drop(1).dropRight(1).replaceAllLiterally("\"\"", "\""))
-	def unquoted: Parser[String] ="""[^,]*""".r
-	class FileFormatException(msg: String) extends Exception(msg)
+  val empty: Company = new Company {
+    val tribes = Nil
+  }
+  def loadFrom(file: File): Company = {
+    val input = Source.fromInputStream(new FileInputStream(file)).getLines.drop(1)
+    val company = new Company.Mutable
+    for (line <- input) {
+      parseAll(record, line) match {
+        case Success((t1, s1, w, s2, t2), _) =>
+          val tribe1 = company.tribe(t1).getOrElse(new Tribe.Mutable(t1))
+          val tribe2 = company.tribe(t2).getOrElse(new Tribe.Mutable(t2))
+          val squad1 = tribe1.squad(s1).getOrElse(new Squad.Mutable(s1))
+          val squad2 = tribe2.squad(s2).getOrElse(new Squad.Mutable(s2))
+          squad1 += squad2 -> w
+          squad2 += squad1 -> w
+          tribe1 += squad1
+          tribe2 += squad2
+          company += tribe1
+          company += tribe2
+        case NoSuccess(msg, _) =>
+          throw new FileFormatException(msg)
+      }
+    }
+    company
+  }
+  class Mutable extends Company {
+    var tribes = List.empty[Tribe.Mutable]
+    override def tribe(name: String) = tribes.find(_.name == name)
+    def +=(tribe: Tribe.Mutable) =
+      if (!tribes.contains(tribe))
+        tribes ::= tribe
+  }
+  class Immutable(val tribes: List[Tribe]) extends Company
+  def apply(tribes: List[Tribe] = Nil): Company = new Immutable(tribes)
+  def record: Parser[(String, String, Int, String, String)] =
+    field ~ "," ~ field ~ "," ~ field ~ "," ~ field ~ "," ~ field ^^ {
+      case t1 ~ _ ~ s1 ~ _ ~ w ~ _ ~ s2 ~ _ ~ t2 =>
+        (t1, s1, w.toInt, s2, t2)
+    }
+  def field: Parser[String] = quoted | unquoted
+  def quoted: Parser[String] ="""("[^"]*")+""".r ^^ (_.drop(1).dropRight(1).replaceAllLiterally("\"\"", "\""))
+  def unquoted: Parser[String] ="""[^,]*""".r
+  class FileFormatException(msg: String) extends Exception(msg)
 }
